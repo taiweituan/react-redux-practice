e5a311b30518191bbe24d7bb95a08163f4fae689
Taiwei Tuan <taiweituan@gmail.com>
2019-02-20 17:57:21 -0600

    proj 5 stuff

--- proj5-streams/client/src/components/App.js
+++ proj5-streams/client/src/components/App.js
@@ -1,25 +1,24 @@
 import React from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Link } from "react-router-dom";
 
-const PageOne = () => {
-    return <div>PageOne</div>;
-};
-
-const PageTwo = () => {
-    return (
-        <div>
-            <button>TEST Page two</button>
-        </div>
-    ); 
-};
+import Header from "./Header";
+import StreamCreate from "./streams/StreamCreate";
+import StreamEdit from "./streams/StreamEdit";
+import StreamDelete from "./streams/StreamDelete";
+import StreamList from "./streams/StreamList";
+import StreamShow from "./streams/StreamShow";
 
 const App = () => {
     return (
         <div>
             <BrowserRouter>
                 <div>
-                    <Route path="/" exact component={PageOne} />    
-                    <Route path="/pagetwo"  component={PageTwo} />    
+                    <Header />
+                    <Route path="/" exact component={StreamList}></Route>
+                    <Route path="/streams/new" component={StreamCreate}></Route>
+                    <Route path="/streams/edit" component={StreamEdit}></Route>
+                    <Route path="/streams/delete" component={StreamDelete}></Route>
+                    <Route path="/streams/show" component={StreamShow}></Route>
                 </div>
             </BrowserRouter>
         </div>
