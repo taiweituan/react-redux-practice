aa705f09add66b42e140cab581c6f893cb39a910
Taiwei Tuan <taiweituan@gmail.com>
2019-02-27 00:24:42 -0600

    createlist router

--- proj5-streams/client/src/components/streams/StreamList.js
+++ proj5-streams/client/src/components/streams/StreamList.js
@@ -1,5 +1,6 @@
 import React from "react";
 import  {connect} from "react-redux";
+import { Link } from "react-router-dom";
 import  { fetchStreams } from "../../actions";
 
 class StreamList extends React.Component {
@@ -39,6 +40,18 @@ class StreamList extends React.Component {
         });
     }
 
+    renderCreate() {
+        if (this.props.isSignedIn) {
+            return (
+                <div>
+                    <Link to="/streams/new" className="ui button primary">
+                        Create Stream
+                    </Link>
+                </div>
+            );
+        }
+    }
+
     render() {
         return (
             <div>
@@ -46,6 +59,7 @@ class StreamList extends React.Component {
                 <div className="ui celled list">
                     {this.renderList()}
                 </div>
+                {this.renderCreate()}
             </div>
         );  
     };
@@ -58,7 +72,8 @@ const mapStateToProps = (state) => {
     // console.log({streams: Object.values(state.streams)}); 
     return {
         streams: Object.values(state.streams),
-        currentUserId: state.auth.userId
+        currentUserId: state.auth.userId,
+        isSignedIn: state.auth.isSignedIn
     };
 };
 export default connect(
